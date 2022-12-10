7d9dd97665a04d484a5a0de1d932b1e3b0f30e99
Hélio <115163244+H3l1oF@users.noreply.github.com>
2022-12-10 09:06:57 +0000

    Add files via upload

---
+++ euromilhoes.cpp
@@ -0,0 +1,46 @@
+// euromilhoes random
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+using namespace std;
+
+void num(int x, int y){
+	int n[x];
+	
+	for (int i=0;i<=x;i++){
+		n[i]=rand() % y + 1;
+		for (int j=0;j<=x;j++)
+    		if (n[i] == n[j] && i!=j){
+        		n[i]=rand() % y + 1;
+        		i=0;
+        	}		
+	}
+	for (int i:n) 
+		cout << i << " ";
+}
+
+
+int main()
+{
+	char o;
+	srand(time(NULL));
+
+	do{
+		cout << "\n*** Euromilhoes ***\n";
+		
+		cout << "\nNumeros:\n ";
+		num(5,50);
+		
+		cout << "\nEstrelas:\n ";
+		num(2,11);
+
+		cout << endl;
+		cout << "\nPrima 0 para sair.\n";
+		cin >> o;
+	}while(o!='0');
+	
+	return 0;
+}
